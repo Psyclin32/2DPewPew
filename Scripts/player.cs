8e7f271558087e74fb9e8535ea0e410d3064ef77
Psyclin32 <162499591+Psyclin32@users.noreply.github.com>
2024-07-02 15:18:57 -0700

    completed basic move controller.

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -5,28 +5,39 @@ using System.Diagnostics;
 public partial class player : CharacterBody2D
 {
 	
-	[Export] public float acc;
-	//public const float Acc = 10.0f;
-	[Export] public int rot_speed;
-
-
-	[Export] public float dampen;
+	[Export] public float acceleration ;
+	//public const float acceleration  = 10.0f;
+	[Export] public int rotation_speed ;
+	[Export] public float dampening;
 	//public const float dampening = 1.0f;	
+	[Export] public float max_speed;
+
 	
 	// public const float JumpVelocity = -400.0f;
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-	float rotation= 0;
-	float rot_dir = 0;
+
+
+
 	public override void _PhysicsProcess(double delta)
 	{	
 		
-		Vector2 velocity = Velocity;
 		float time =  (float)delta;
-		//float rot_dir = 0f;
-		float dir = Input.GetAxis( "Rotate Right","Rotate Left");
 		
-		rot_dir += dir; 
+		Vector2 inputs = new Vector2(0f, Input.GetAxis("Forward", "Reverse"));
+		Velocity += inputs.Rotated(Rotation) * acceleration ;
+		//Velocity = Velocity.LimitLength(max_speed);
+		Rotate(Mathf.DegToRad(Input.GetAxis( "Rotate Left","Rotate Right")*rotation_speed *time));
+		
+		if (inputs.Y == 0){
+			Velocity = Velocity.MoveToward(Vector2.Zero,dampening);
+		}
+	
+		MoveAndSlide();
+	}
+}
+//CODE GRAVEYARD 
+//rot_dir += dir; 
 		
 		//Debug.Print("Rotation reading as: " + rot_dir);
 		
@@ -40,9 +51,10 @@ public partial class player : CharacterBody2D
 		}*/
 
 
+		/*
 		if (Input.IsActionPressed("Forward")){
 			//Debug.Print("Forward Pressed!");
-			velocity.X += acc*time; 
+			velocity.X += acceleration eleration *time; 
 		}
 		else{
 			velocity.X -= dampen*time;
@@ -50,12 +62,4 @@ public partial class player : CharacterBody2D
 				velocity.X = 0;	
 			}
 		}
-		
-		Debug.Print("Velocity before rotation:" + velocity);
-
-		rotation += rot_speed * rot_dir * time;
-		Velocity = velocity.Rotated(rotation);
-		Debug.Print("Velocity post rotation:" + Velocity);
-		MoveAndSlide();
-	}
-}
+		*/
