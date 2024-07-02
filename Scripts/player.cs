2a972342690e2d6829303f6a99bbc40c1dd7edf5
Psyclin32 <162499591+Psyclin32@users.noreply.github.com>
2024-07-02 14:13:42 -0700

    Player controller changes
    rotation is being wonky ):

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -4,34 +4,58 @@ using System.Diagnostics;
 
 public partial class player : CharacterBody2D
 {
-	public const float Acc = 10.0f;
+	
+	[Export] public float acc;
+	//public const float Acc = 10.0f;
+	[Export] public int rot_speed;
+
+
+	[Export] public float dampen;
+	//public const float dampening = 1.0f;	
+	
 	// public const float JumpVelocity = -400.0f;
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
+	float rotation= 0;
+	float rot_dir = 0;
 	public override void _PhysicsProcess(double delta)
-	{
+	{	
+		
 		Vector2 velocity = Velocity;
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		// Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		// if (direction != Vector2.Zero)
-		// {
-		// 	velocity.X = direction.X * Acc;
-		// }
-		// else
-		// {
-		// 	velocity.X = Mathf.MoveToward(Velocity.X, 0, Acc);
-		// }
-		if (Input.IsActionPressed("Forward")){
-			Debug.Print("Forward Pressed!");
+		float time =  (float)delta;
+		//float rot_dir = 0f;
+		float dir = Input.GetAxis( "Rotate Right","Rotate Left");
+		
+		rot_dir += dir; 
+		
+		//Debug.Print("Rotation reading as: " + rot_dir);
+		
+		/*
+		if (Input.IsActionPressed("Rotate Right")){
+		rot_dir += 1;
 		}
 
+		if (Input.IsActionPressed("Rotate Right")){
+		rot_dir -= 1;
+		}*/
 
 
+		if (Input.IsActionPressed("Forward")){
+			//Debug.Print("Forward Pressed!");
+			velocity.X += acc*time; 
+		}
+		else{
+			velocity.X -= dampen*time;
+			if(velocity.X <= (float)(0.0)){
+				velocity.X = 0;	
+			}
+		}
+		
+		Debug.Print("Velocity before rotation:" + velocity);
 
-
-		Velocity = velocity;
+		rotation += rot_speed * rot_dir * time;
+		Velocity = velocity.Rotated(rotation);
+		Debug.Print("Velocity post rotation:" + Velocity);
 		MoveAndSlide();
 	}
 }
