3b4c7f021442c07a6217d42e27d07c4f8d07cf75
Psyclin32 <162499591+Psyclin32@users.noreply.github.com>
2024-07-04 11:06:27 -0700

    Bullet Projectile Added on attack click.
    Check comments in code.

--- Scripts/Bullet.cs
+++ Scripts/Bullet.cs
@@ -1,15 +1,18 @@
 using Godot;
 using System;
 
-public partial class Bullet : Node2D
+public partial class Bullet : RigidBody2D  //Needs to extend to get physics methods when object called. 
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-	{
+	{	
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-	{
+	{			
+
+		
 	}
 }
--- Scripts/player.cs
+++ Scripts/player.cs
@@ -1,7 +1,8 @@
 using Godot;
 using System;
-using System.Diagnostics;
-using System.Security.Cryptography.X509Certificates;
+//using System.Collections.ObjectModel;
+//using System.Diagnostics;
+//using System.Security.Cryptography.X509Certificates;
 
 public partial class player : CharacterBody2D
 {
@@ -23,24 +24,26 @@ public partial class player : CharacterBody2D
 
 
 
-	private bool ready_fire = false; 
-	private PackedScene _bulletScene = GD.Load<PackedScene>("res://Scenes/Bullet.tscn"); 
+	private bool ready_fire = false;
+	private PackedScene _bulletScene = GD.Load<PackedScene>("res://Scenes/Bullet.tscn");
 
 	public override void _Ready()
 	{
 		var timer = GetNode<Timer>("WeaponTimer");
 		timer.Timeout += OnWeaponTimerTimeout;
-	
+
 
 		base._Ready();
 	}
 
 	public override void _Process(double delta)
-	{	
+	{
 		//Declare local nodes
-		var gun_Barrel = GetNode<Marker2D>("GunBarrelPos");
+
 		var Weapon = GetNode<Sprite2D>("Weapon");
-		
+		var gun_Barrel = Weapon.GetNode<Marker2D>("GunBarrelPos");
+		//gun_Barrel = GetNode<Marker2D>("GunBarrelPos");
+
 		//Weapon turret looking at cursor
 		Weapon.LookAt(GetGlobalMousePosition()); //Lookat requires Global Coords of mouse pointers
 		Weapon.Rotate(MathF.PI / 2); //current asset is -90deg off, needs dynamic rotation to keep accurate. Rotation in code is in Radians thus PI/2.  
@@ -49,7 +52,7 @@ public partial class player : CharacterBody2D
 		if (Input.IsActionJustPressed("Fire Weapon") & ready_fire)
 		{
 			//Spawn in bullet instance then disable based on ROF with timer. 
-			fire_Weapon(gun_Barrel.Position);
+			fire_Weapon(gun_Barrel.GlobalPosition); //Marker needs to be child of weapon to properly track rotation
 			ready_fire = false;
 			//Debug.Print("Weapon Fired!");
 		}
@@ -58,18 +61,18 @@ public partial class player : CharacterBody2D
 
 	private void fire_Weapon(Vector2 pos)
 	{
-		 RigidBody2D projectile = (RigidBody2D) _bulletScene.Instantiate();
-		 projectile.Visible = false;
-		 projectile.Position = pos;
-		 projectile.LookAt(GetGlobalMousePosition());
-		 projectile.LinearVelocity += Velocity;
-		 AddChild(projectile);
-		 
+
+		RigidBody2D projectile = (RigidBody2D)_bulletScene.Instantiate(); //cast resource as Rigid Body. Associated Nodes script must extend the Node version we want to upack at its top level. 
+		projectile.Position = pos;  // gun_barrel POS
+		projectile.LookAt(GetGlobalMousePosition()); //rotate the sprite to point at mouse
+		projectile.LinearVelocity = Velocity + pos.DirectionTo(GetGlobalMousePosition()) * 400.0f;  // sets initial velocity. Currently accounts for ship velocity. Realistic but maybe not good for UX.																									
+		AddSibling(projectile); //DO NOT WANT AS CHILD TO PLAYER. BINDS ROTATION TO PLAYER CONTROLS. 
+
 
 	}
 
 	private void OnWeaponTimerTimeout()
-	{	
+	{
 		ready_fire = true;
 		/* if for debug only
 		if(!ready_fire)
